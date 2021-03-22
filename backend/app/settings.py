62e4346bd3d26bcd5296fe22bd14da38bb900d73
zequi <calvo.ezequias@gmail.com>
2021-03-22 14:41:00 -0300

    createdsuperuser

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the app like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-#comment
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
