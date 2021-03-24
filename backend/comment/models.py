b2d95cc35672652804181345c0eecb96cc8edb51
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-03-24 18:37:04 +0100

    all models created. developed minor headache, still dont like django

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from backend.post.models import Post
+from backend.userprofile.models import Userprofile
+
+
+class Comment(models.Model):
+    content = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    created_by = models.ForeignKey(
+        to=Userprofile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='comments'
+    )
+
+    comment_on_post = models.ForeignKey(
+        to=Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    changed_when = models.DateTimeField(auto_now=True)
+    created_when = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f' comment written by:{self.created_by}'
--- backend/userprofile/models.py
+++ backend/userprofile/models.py
@@ -1,3 +1,51 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+User = settings.AUTH_USER_MODEL
+
+
+class Userprofile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name='userprofile')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=255, blank=True)
+    about = models.TextField(blank=True)
+    hobbies = models.TextField(blank=True)
+
+    friend = models.ManyToManyField(
+        to='self',
+        related_name='my_friend',
+        blank=True,
+        symmetrical=True
+    )
+
+    i_follow = models.ManyToManyField(
+        to='self',
+        related_name='my_followers',
+        blank=True,
+        symmetrical=False
+    )
+
+    my_followers = models.ManyToManyField(
+        to='self',
+        related_name='i_follow',
+        blank=True,
+        symmetrical=False
+    )
+
+    changed_when = models.DateTimeField(auto_now=True)
+    created_when = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f'{self.id}: Profile from {self.user.username}'
+
+    @receiver(post_save, sender=User)
+    def create_registration_profile(sender, instance, **kwargs):
+        profile, created = Userprofile.objects.get_or_create(user=instance)
+        if created:
+            profile.save()
