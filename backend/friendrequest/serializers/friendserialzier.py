719c404d98fed3b850a727dbd7456b823a21d599
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-03-25 17:34:36 +0100

    friend serializer

---
+++ backend/friendrequest/serializers/friendserialzier.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from friendrequest.models import FriendRequest
+
+
+class Friend_serializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = FriendRequest
+        fields = '__all__'
--- backend/friendrequest/views.py
+++ backend/friendrequest/views.py
@@ -1,16 +1,22 @@
-# from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
 from userprofile.models import Userprofile
+from friendrequest.serializers.friendserialzier import Friend_serializer
 
+from backend.friendrequest.models import FriendRequest
 
 class Create_friendship(CreateAPIView):
     serializer_class = Friend_serializer
     queryset = Userprofile.objects.all()
 
     def create(self, request, *args, **kwargs):
-        sender = request.user.username
+        sender = request.user.userprofile
         receiver = self.get_object
-        friend_request =
+        if sender == receiver:
+            return JsonResponse({'detail': 'no hedonism!'})
+
+        friend_request = FriendRequest(sender=sender, receiver=receiver)
+        friend_request.save()
+        return Response(self.get_serializer(friend_request).data)
