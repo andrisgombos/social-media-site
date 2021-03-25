e9fe9d6383556aa8c5e3b68676ddeb7cc141ab84
Mario Marchiorello <mario.marchiorello@gmail.com>
2021-03-25 16:59:02 +0100

    test login locally

--- backend/friendrequest/views.py
+++ backend/friendrequest/views.py
@@ -1,3 +1,16 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import CreateAPIView
+
+from userprofile.models import Userprofile
+
+
+class Create_friendship(CreateAPIView):
+    serializer_class = Friend_serializer
+    queryset = Userprofile.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        sender = request.user.username
+        receiver = self.get_object
+        friend_request =
